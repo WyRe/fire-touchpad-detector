f49cec0f8b9f0d2908b74f9cfbc90eaa05eead83
Javier <jfernandil@alumnos.unex.es>
2019-01-10 21:26:55 +0100

    Adding third measure
    Humidity has been added to device.
    
    Also has been fixed a few comments and
    now each button shows the measurement units.
    
    A few dummy variables have been modified like constants
    values.

--- src/src.ino
+++ src/src.ino
@@ -27,45 +27,58 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 // Size of the color selection boxes and the paintbrush size
 #define BOXSIZE 40            // both buttons have the same height and width, using this parameter
 
-// Buttons and Text Positions
-#define TEMP_BUTT_POSx 50
+// Button's Texts Positions
+#define TEMP_BUTT_POSx 25
 #define TEMP_BUTT_POSy 18
 #define TEMP_BUTT_COL ILI9341_WHITE
 #define TEMP_BUTT_SIZ 0.5
-#define GAS_BUTT_POSx 170     // 20*8.5 is also compiled
+#define GAS_BUTT_POSx 97.5
 #define GAS_BUTT_POSy 18
 #define GAS_BUTT_COL ILI9341_WHITE
 #define GAS_BUTT_SIZ 0.5
+#define HUM_BUTT_POSx 175
+#define HUM_BUTT_POSy 18
+#define HUM_BUTT_COL ILI9341_WHITE
+#define HUM_BUTT_SIZ 0.5
 
-#define TEMP_MEAS_POSx 60
-#define TEMP_MEAS_POSy 150
+// Measurement position, size and colours
+#define MEAS_POSx 115
+#define MEAS_POSy 150
+#define MEAS_SIZ 2
 #define TEMP_MEAS_COL ILI9341_RED
-#define TEMP_MEAS_SIZ 2
-#define GAS_MEAS_POSx 100
-#define GAS_MEAS_POSy 150
+#define HUM_MEAS_COL ILI9341_BLUE
 #define GAS_MEAS_COL ILI9341_GREEN
-#define GAS_MEAS_SIZ 2
 
-// Slider parameters
+
+// Slider position parameters
 #define SLID_INIT_POSx 30
 #define SLID_INIT_POSy 270
 #define SLID_WIDTHx 180
 #define SLID_HEIGHTy 15
 
 
-int oldmeasure, currmeasure;            // ----- temperature:1 ----- gas:2 -----
-
 // Right limiter for slider.
-int lim_posx = SLID_INIT_POSx + SLID_WIDTHx;
-int lim_posy = SLID_INIT_POSy;
-int lim_wid = tft.width() - SLID_INIT_POSx - SLID_WIDTHx;
-int lim_h = SLID_HEIGHTy;
+#define lim_posx SLID_INIT_POSx + SLID_WIDTHx
+#define lim_posy SLID_INIT_POSy
+#define lim_wid tft.width() - SLID_INIT_POSx - SLID_WIDTHx
+#define lim_h SLID_HEIGHTy
+
+
+// Chosen limit by user position
+#define LIM_POSx SLID_INIT_POSx + 75
+#define LIM_POSy SLID_INIT_POSy - 20
+#define LIM_SIZ 2
+
+
+// Possible values -----> temperature:1 ----- humidity:2 ----- gas:3 ------
+int oldmeasure, currmeasure;
+
 
 // Selected value.
 String pax = String(0);
 
 
-void setup(void) {
+void setup() {
 
   // Starting display (tft), touchscreen (ts) and serial comm. 
   Serial.begin(9600);
@@ -83,18 +96,22 @@ void setup(void) {
   tft.fillScreen(ILI9341_BLACK);
 
   // Displaying text of buttons measure selection boxes                             // Modify defined constants
-  printText(TEMP_BUTT_POSx, TEMP_BUTT_POSy, "Temp", TEMP_BUTT_COL, TEMP_BUTT_SIZ);
-  printText(GAS_BUTT_POSx, GAS_BUTT_POSy, "Gas", GAS_BUTT_COL, GAS_BUTT_SIZ);
+  printText(TEMP_BUTT_POSx, TEMP_BUTT_POSy, "T [C]", TEMP_BUTT_COL, TEMP_BUTT_SIZ);
+  printText(GAS_BUTT_POSx, GAS_BUTT_POSy, "Hum [%]", GAS_BUTT_COL, GAS_BUTT_SIZ);
+  printText(HUM_BUTT_POSx, HUM_BUTT_POSy, "Gas [ppm]", HUM_BUTT_COL, HUM_BUTT_SIZ);
 
   // Select the current measure 'Gas'; modify currmeasure value to setup a default measure to show
   // in this case it draws a white rectancle around the button
-  currmeasure = 2;
+  currmeasure = 3;
   if (currmeasure == 1) {
-    tft.drawRect(0, 0, BOXSIZE*3, BOXSIZE, ILI9341_WHITE);
-    printText(TEMP_MEAS_POSx, TEMP_MEAS_POSy, "Temperature", TEMP_MEAS_COL, TEMP_MEAS_SIZ);
+    tft.drawRect(0, 0, BOXSIZE*2, BOXSIZE, ILI9341_WHITE);
+    printInteg(MEAS_POSx, MEAS_POSy, currmeasure, TEMP_MEAS_COL, MEAS_SIZ);
   } else if (currmeasure == 2) {
-      tft.drawRect(BOXSIZE*3, 0, BOXSIZE*3, BOXSIZE, ILI9341_WHITE);
-      printText(GAS_MEAS_POSx, GAS_MEAS_POSy, "GAS", GAS_MEAS_COL, GAS_MEAS_SIZ);
+      tft.drawRect(BOXSIZE*2,0,BOXSIZE*2, BOXSIZE, ILI9341_WHITE);
+      printInteg(MEAS_POSx, MEAS_POSy, currmeasure, HUM_MEAS_COL, MEAS_SIZ);
+  } else if (currmeasure == 3) {
+      tft.drawRect(BOXSIZE*4, 0, BOXSIZE*2, BOXSIZE, ILI9341_WHITE);
+      printInteg(MEAS_POSx, MEAS_POSy, currmeasure, GAS_MEAS_COL, MEAS_SIZ);
   }
   // Drawing the slider border
   tft.drawRect(SLID_INIT_POSx - 1, SLID_INIT_POSy - 1, SLID_WIDTHx + 1, SLID_HEIGHTy + 2, ILI9341_WHITE);
@@ -122,25 +139,33 @@ void loop() {
   // then, draws a white rectangle and displays the measure coming from any kind of sensor
   if (p.y < BOXSIZE && p.y > 0) {
     oldmeasure = currmeasure;
-    if (p.x < BOXSIZE*3) {
+    if (p.x < BOXSIZE*2) {
       currmeasure = 1;
-      printText(GAS_MEAS_POSx, GAS_MEAS_POSy, "GAS", ILI9341_BLACK, GAS_MEAS_SIZ);
-      tft.drawRect(0, 0, BOXSIZE*3, BOXSIZE, ILI9341_WHITE);
+      printInteg(MEAS_POSx, MEAS_POSy, oldmeasure, ILI9341_BLACK, MEAS_SIZ);
+      tft.drawRect(0, 0, BOXSIZE*2, BOXSIZE, ILI9341_WHITE);
       Serial.println("TEMP");
-      printText(TEMP_MEAS_POSx, TEMP_MEAS_POSy, "Temperature", TEMP_MEAS_COL, TEMP_MEAS_SIZ);
-    } else if (p.x < BOXSIZE*6) {
+      printInteg(MEAS_POSx, MEAS_POSy, currmeasure, TEMP_MEAS_COL, MEAS_SIZ);
+    } else if(p.x > BOXSIZE*2 && p.x < BOXSIZE*4) {
         currmeasure = 2;
-        printText(TEMP_MEAS_POSx, TEMP_MEAS_POSy, "Temperature", ILI9341_BLACK, TEMP_MEAS_SIZ);
-        tft.drawRect(BOXSIZE*3, 0, BOXSIZE*3, BOXSIZE, ILI9341_WHITE);
+        printInteg(MEAS_POSx, MEAS_POSy, oldmeasure, ILI9341_BLACK, MEAS_SIZ);
+        tft.drawRect(BOXSIZE*2, 0, BOXSIZE*2, BOXSIZE, ILI9341_WHITE);
+        Serial.println("HUM");
+        printInteg(MEAS_POSx, MEAS_POSy, currmeasure, HUM_MEAS_COL, MEAS_SIZ);
+    } else if (p.x > BOXSIZE*4) {
+        currmeasure = 3;
+        printInteg(MEAS_POSx, MEAS_POSy, oldmeasure, ILI9341_BLACK, MEAS_SIZ);
+        tft.drawRect(BOXSIZE*4, 0, BOXSIZE*2, BOXSIZE, ILI9341_WHITE);
         Serial.println("GAS");
-        printText(GAS_MEAS_POSx, GAS_MEAS_POSy, "GAS", GAS_MEAS_COL, GAS_MEAS_SIZ);
+        printInteg(MEAS_POSx, MEAS_POSy, currmeasure, GAS_MEAS_COL, MEAS_SIZ);
     }
    // This following block fixes the previous button border by drawing a black rectangle 
     if (oldmeasure != currmeasure) {
       if (oldmeasure == 1)
-        tft.drawRect(0, 0, BOXSIZE*3, BOXSIZE, ILI9341_BLACK);
+        tft.drawRect(0, 0, BOXSIZE*2, BOXSIZE, ILI9341_BLACK);
       if (oldmeasure == 2)
-        tft.drawRect(BOXSIZE*3, 0, BOXSIZE*3, BOXSIZE, ILI9341_BLACK);
+        tft.drawRect(BOXSIZE*2, 0, BOXSIZE*2, BOXSIZE, ILI9341_BLACK);
+      if (oldmeasure == 3) 
+        tft.drawRect(BOXSIZE*4, 0, BOXSIZE*2, BOXSIZE, ILI9341_BLACK);  
     }
   }
 
@@ -157,16 +182,12 @@ void loop() {
       // Filling right and left parts of slider 
       tft.fillRect(SLID_INIT_POSx, SLID_INIT_POSy, p.x, SLID_HEIGHTy, ILI9341_GREEN);
       tft.fillRect(p.x, SLID_INIT_POSy, corr_wid, SLID_HEIGHTy, ILI9341_RED);
-      // Fixing the right side
+      // Fixing the right side and white border
       tft.fillRect(lim_posx, lim_posy, lim_wid, lim_h, ILI9341_BLACK);
       tft.drawRect(SLID_INIT_POSx - 1, SLID_INIT_POSy - 1, SLID_WIDTHx + 1, SLID_HEIGHTy + 2, ILI9341_WHITE);
 
       // This part draws the limit chosen by the user by dragging the slider. If the board read a value higher than this limit 
-      // from any sensor It will trigger an alarm. However, by the moment is just to draw the chosen limit in the display 
-      int LIM_POSx = SLID_INIT_POSx + 75;
-      int LIM_POSy = SLID_INIT_POSy - 20;
-      int LIM_SIZ = 2;
-      
+      // from any sensor It will trigger an alarm. However, by the moment is just to draw the chosen limit in the display.
       // Here we must compute with p.x to translate the value to different scales, anda maybe make a case, to display the 
       // right scale (depending on the measure). By the moment it is just showing the p.x value like a text string
       printText(LIM_POSx, LIM_POSy, pax, ILI9341_BLACK, LIM_SIZ);
@@ -177,13 +198,23 @@ void loop() {
 } // void loop() end.
 
 // In this declarative version this is the unique one function that We've used. It display a given string of characteres. 
-// Maybe is not much efficient, cause a string requires much more allocated memmory than a integer. This MUST BE MODIFIED when finally 
-// the display begins to show measures and limits as integers. 
+// Maybe is not much efficient, cause a string requires much more allocated memmory than a integer.
 unsigned long printText(int x, int y, String strg, int col, int sz) {
   unsigned long start = micros();
   //tft.setRotation(1);         // The drawn value can be rotaded
   tft.setCursor(x, y);
   tft.setTextColor(col); tft.setTextSize(sz);
   tft.println(strg);
+  return micros() - start;
+}
+
+// This function is very similar to previous one, but in this case we are printing an integer, lighter than a string. Apparently 
+// the performing is not being affected
+unsigned long printInteg(int x, int y, int val, int col, int sz) {
+  unsigned long start = micros();
+  //tft.setRotation(1);         // The drawn value can be rotaded
+  tft.setCursor(x, y);
+  tft.setTextColor(col); tft.setTextSize(sz);
+  tft.println(val);
   return micros() - start;
 }
